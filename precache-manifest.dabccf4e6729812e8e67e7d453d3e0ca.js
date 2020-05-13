b2499cac77d7089dd2f83198cfad66cf9f9fbbe8
glevon <levon77777@gmai.com>
2020-05-13 21:42:38 +0400

    Updates

---
+++ precache-manifest.dabccf4e6729812e8e67e7d453d3e0ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62f8a9336d2bc9eed544e6ac269ff64",
+    "url": "/tnayin4/index.html"
+  },
+  {
+    "revision": "c35cff5aacc30e4e1e26",
+    "url": "/tnayin4/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "087598f6c207c2c696c8",
+    "url": "/tnayin4/static/js/2.2e8f7ac8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tnayin4/static/js/2.2e8f7ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35cff5aacc30e4e1e26",
+    "url": "/tnayin4/static/js/main.3ce5aff7.chunk.js"
+  },
+  {
+    "revision": "5144da6da21b4f94eab3",
+    "url": "/tnayin4/static/js/runtime-main.0b378405.js"
+  }
+]);
